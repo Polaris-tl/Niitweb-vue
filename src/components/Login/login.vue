83bced08d842616dee7a435f3b01ac0a5c52ccb7
Polaris_tl <polaris66tl@gmail.com>
2019-12-19 11:19:56 +0800

    新闻页面添加登录验证功能

---
+++ src/components/Login/login.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="login">
+    <h4>查看该板块需要权限，请登录</h4>
+    <form >
+      <p>用户名：111；密码：222</p>
+      <div>
+        <label for="user">用户名</label>
+        <input id='user' type="text" v-model="name"/>
+      </div>
+      <div>
+        <label for="pwd">密码</label>
+        <input id='pwd' type="password" v-model="pwd"/>
+      </div>
+      <p v-if="error" class="err-msg">{{error}}</p>
+      <button @click.prevent="login">提交</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          name: '',
+          pwd : '',
+          error : ''
+      }
+  },
+
+  methods : {
+      check(name,pwd) {
+          if(!name || !pwd) {
+              this.error = '请输入用户名或密码'
+              return false
+          }
+          return true
+      },
+      login() {
+          const {name, pwd, $router } = this
+          if(!this.check(name,pwd)) return
+          if(name == '111' && pwd == '222'){
+              sessionStorage.setItem("isLogin",true)
+              $router.push({ name : 'xinwen' })
+          } else {
+              this.error = '密码或用户名错误'
+          }
+      }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.login {
+    width: 300px;
+    margin: 10% auto;
+    text-align: center;
+    padding: 16px;
+    border-radius: 4px;
+    background: #def0ff;
+  h4{
+    font-size: 21px;
+    color: #565656;
+    margin-bottom: 20px;
+  }
+  input{
+    margin: 6px;
+    height: 29px;
+    font-weight: normal;
+    display: inline-block;
+    border: none;
+  }
+  p{
+    padding-bottom: 19px;
+    color: #ff2020;
+  }
+  button{
+    display: inline-block;
+    border: none;
+    padding: 6px 37px;
+    background: #42aefb;
+    color: #fff;
+    margin-top: 10px;
+  }
+  div{
+    position: relative;
+  }
+  label{
+    position: absolute;
+    left: -5px;
+    top: 9px;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -20,20 +20,30 @@ const Xueyuan = () => import(/* webpackChunkName: "tangling2" */ '../components/
 const Xinwen = () => import(/* webpackChunkName: "tangling3" */ '../components/Xinwen/Xinwen.vue')  
 const NewsList = () => import(/* webpackChunkName: "tangling3" */ '../components/Xinwen/NewsList.vue')  
 const NewsDetail = () => import(/* webpackChunkName: "tangling4" */ '../components/Xinwen/NewsDetail.vue')  
+const Login = () => import(/* webpackChunkName: "login" */ '../components/Login/login.vue')  
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: Index},
+  { path: '/login', component: Login},
   { path: '/jianjie', component: Jianjie },
   { path: '/shizhi', component: Shizi },
   { path: '/luxian', component: Luxian },
   { path: '/zhuanye', component: Zhuanye },
-  { path: '/xueyuan', component: Xueyuan },
-  { path: '/xinwen', component: Xinwen ,
+  { path: '/xueyuan',
+    component: Xueyuan,
+  },
+  { path: '/xinwen', 
+    name:'xinwen',  
+    component: Xinwen ,
+    redirect:'/xinwen/list',
+    meta:{
+      requireAuth:true
+    },
     children: [
       {
-        path: '',
+        path: 'list',
         component: NewsList
       },
       {
@@ -49,5 +59,29 @@ const router = new VueRouter({
   routes,
   linkActiveClass:'active'
 })
+
+router.beforeEach((to, from, next) => {
+  //通过sessionStorage判断是否登录
+  var isLogin = sessionStorage.getItem('isLogin') || '' 
+  //检查路由meta是否包含了requireAuth = true 字段
+  var isParentNeedAuth = to.matched.some((item) => { //嵌套路由的父路由设置了 requireAuth = true 时，所有的子路由就不用再设置了 requireAuth = true ，他们都需要验证
+    return item.meta.requireAuth == true
+  })
+
+  if (isParentNeedAuth) { //需要验证时，再判断登录与否
+    if(!isLogin){ //没有登录，跳转登录页面
+      next({
+        path: '/login',
+      })
+    }else{ //登录了，跳转目标页面
+      next();
+    }
+  }else { //不需要验证时，跳转目标页面
+    next();
+  }
+})
+router.afterEach(() => {
+  window.scroll(0, 30);
+});
 
 export default router
